174686b0fc59c1ac8c3f8ba41f1821f10faced22
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-29 09:32:50 +0900

    :fire: (Swagger) 불필요한 Tag 삭제

--- src/config/swagger/swagger.ts
+++ src/config/swagger/swagger.ts
@@ -5,7 +5,6 @@ export const setupSwagger = (app: INestApplication): void => {
   const config = new DocumentBuilder()
     .setTitle('땡겨')
     .setDescription('Team 오합지존의 땡겨 프로젝트를 위한 API 문서')
-    .addTag('Ohapzizon')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
