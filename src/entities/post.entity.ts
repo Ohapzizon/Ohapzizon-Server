9eb3d0514b22d3b1825e2a81afee7ba4e01ef8af
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-29 16:44:46 +0900

    ✨(Post) Post Entity 생성

---
+++ src/entities/post.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('post')
+export default class Post extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'post_idx'})
+  idx: number;
+
+  @Column({ name: 'post_title' })
+  title: string;
+
+  @Column({ name: 'head_count' })
+  headCount: number;
+
+  @Column({ name: 'post_contents' })
+  contents: string;
+
+  @Column({ name: 'is_day_and_night' })
+  isDayAndNight: Date;
+}
