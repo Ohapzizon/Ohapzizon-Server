8a8246d8febd1d89038429469bc13e5eaf21392c
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-29 17:04:10 +0900

    :art: (PostRepository) prettier!!

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -2,4 +2,4 @@ import { EntityRepository, Repository } from 'typeorm';
 import Post from '../entities/post.entity';
 
 @EntityRepository(Post)
-export class PostRepository extends Repository<Post>{}
+export class PostRepository extends Repository<Post> {}
