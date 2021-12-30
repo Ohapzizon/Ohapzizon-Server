435a84ef7b5873e6a08cacb0dee50419bf681152
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-30 12:15:48 +0900

    :sparkles: (User) add User entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import Recruit from './recruit.entity';
+
+@Entity('user')
+export default class User {
+  @PrimaryGeneratedColumn({ name: 'user_idx' })
+  id: number;
+
+  @Column({ unique: true, nullable: false, name: 'email' })
+  email: string;
+
+  @Column({ unique: true, name: 'username' })
+  username: string;
+
+  @Column({ nullable: false, name: 'password' })
+  password: string;
+
+  @OneToMany(() => Recruit, (recruit) => recruit.user)
+  recruit: Recruit;
+}
