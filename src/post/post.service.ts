0086b54bf92f61ae00ae06d8b30c05e149f04b31
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-30 11:31:22 +0900

    :sparkles: (PostService) add posting method

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PostRepository } from './post.repository';
+import { PostDto } from './dto/post.dto';
+import Post from '../entities/post.entity';
 
 @Injectable()
 export class PostService {
   constructor(private readonly postRepository: PostRepository) {}
+
+  async posting({ title, contents, headCount, isDayAndNight }: PostDto) {
+    const hour = new Date().getHours();
+    isDayAndNight = await this.isDayCheck(hour);
+    const post: Post = this.postRepository.create({
+      title: title,
+      contents: contents,
+      headCount: headCount,
+      isDayAndNight: isDayAndNight,
+    });
+    return await this.postRepository.save(post);
+  }
+
+  async isDayCheck(hour: number) {
+    if (8 <= hour && hour <= 14) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
