3a667d85c27d6670ef49fd7a5fd36b64f0424cde
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-31 15:49:58 +0900

    :sparkles: (AuthModule) GoogleStrategy 및 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleStrategy } from './strategy/google.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../user/user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository]), PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy],
 })
 export class AuthModule {}
