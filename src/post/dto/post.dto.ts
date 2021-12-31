aaaa83a74cc8b3c4a836c8e56bd0560b7c53c78f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-31 14:32:58 +0900

    :triangular_flag_on_post: (PostDto) Not used field remove

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -3,10 +3,6 @@ import { IsBoolean, IsNumber, IsString } from 'class-validator';
 export class PostDto {
   @IsString()
   title: string;
-  @IsNumber()
-  headCount: number;
   @IsString()
   contents: string;
-  @IsBoolean()
-  isDayAndNight: boolean;
 }
