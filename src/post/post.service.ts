9179b5318dacf67f23311cc26d0016a1bc8fed5c
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-31 14:30:53 +0900

    :triangular_flag_on_post: (PostService) Using enums to post
    DayOrNight를 enum으로 선언하여 조건에 따라 day와 night를 반환하는 로직으로 변경하였습니다

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,24 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { PostRepository } from './post.repository';
 import { PostDto } from './dto/post.dto';
 import Post from '../entities/post.entity';
+import { DayOrNight } from '../entities/day_or_night.enum';
 
 @Injectable()
 export class PostService {
   constructor(private readonly postRepository: PostRepository) {}
 
-  async posting({ title, contents, headCount, isDayAndNight }: PostDto) {
-    const hour = new Date().getHours();
-    isDayAndNight = await this.isDayCheck(hour);
+  async posting({ title, contents }: PostDto) {
+    const isDayOrNight: DayOrNight = await this.isDayCheck();
+    const date = new Date().toISOString().slice(0, 10);
     const post: Post = this.postRepository.create({
       title: title,
       contents: contents,
-      headCount: headCount,
-      isDayAndNight: isDayAndNight,
+      isDayOrNight: isDayOrNight,
+      createdAt: date,
     });
     return await this.postRepository.save(post);
   }
 
-  async isDayCheck(hour: number) {
-    return 0 <= hour && hour <= 13;
+  async isDayCheck() {
+    const hour = new Date().getHours();
+    if (0 <= hour && hour <= 13) {
+      return DayOrNight.DAY;
+    } else return DayOrNight.NIGHT;
   }
+
+  async findPostAll(id: number) {}
 }
