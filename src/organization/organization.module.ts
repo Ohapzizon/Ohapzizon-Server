29690b342c819191a73f904b778a139e88ef6dd3
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-01-02 18:03:25 +0900

    ✨(OrganizationModule) add Module

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationRepository } from './organization.repository';
+import { OrganizationService } from './organization.service';
+import { UserRepository } from '../user/user.repository';
+import { PostRepository } from '../post/post.repository';
+import { GroupService } from '../group/group.service';
+import { OrganizationController } from './organization.controller';
+import { GroupRepository } from '../group/group.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrganizationRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      OrganizationRepository,
+      PostRepository,
+      UserRepository,
+      GroupRepository,
+    ]),
+  ],
+  controllers: [OrganizationController],
+  providers: [OrganizationService, GroupService],
+
 })
 export class OrganizationModule {}
