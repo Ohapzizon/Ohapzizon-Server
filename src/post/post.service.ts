3a7037e77b0ac4994ec9404a71df4fd60f4e24dd
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-01-02 14:43:29 +0900

    🚩(PostService) add findAllPost method

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { PostRepository } from './post.repository';
 import { PostDto } from './dto/post.dto';
 import Post from '../entities/post.entity';
+import { OrganizationRepository } from '../organization/organization.repository';
+import { FindPostDto } from './dto/findPost.dto';
+import { getRepository } from 'typeorm';
+import Organization from '../entities/organization.entity';
+import { PostDto } from './dto/post.dto';
 
 @Injectable()
 export class PostService {
@@ -21,5 +26,34 @@ export class PostService {
 
   async isDayCheck(hour: number) {
     return 0 <= hour && hour <= 13;
+  }
+
+  async findAllPost() {
+    const data = await this.getPosts();
+    let post = new FindPostDto();
+    const result: FindPostDto[] = [];
+    const name: string[] = [];
+
+    data.forEach((value) => {
+      post = value;
+      value.peopleList.forEach((value) => {
+        name.push(value);
+      });
+      post.peopleList = name;
+      result.push(post);
+    });
+    return result;
+  }
+
+  async getPosts() {
+    return getRepository(Organization)
+      .createQueryBuilder('o')
+      .select(
+        'post_idx, post_title, post_contents, is_day_and_night, created_at, max_count, u.name "peopleList", pu.name "author"',
+      )
+      .innerJoin('o.post', 'p')
+      .innerJoin('o.user', 'u')
+      .innerJoin('p.user', 'pu')
+      .getRawMany();
   }
 }
