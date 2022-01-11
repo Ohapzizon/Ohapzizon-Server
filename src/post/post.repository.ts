f7608336d89b0d19d149515d50b906c5d443b708
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-01-11 18:45:15 +0900

    :recycle: (Post) 예외처리, 코드 리팩토링
    clean code를 위한 상세조회를 게시글 상세조회와 신청 목록 조회로 나누었습니다

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -1,6 +1,13 @@
-import { EntityRepository, FindOneOptions, Repository } from 'typeorm';
+import {
+  EntityRepository,
+  FindOneOptions,
+  getRepository,
+  Repository,
+} from 'typeorm';
 import Post from '../entities/post.entity';
 import { NotFoundException } from '@nestjs/common';
+import Organization from '../entities/organization.entity';
+import { PostDto } from './dto/post.dto';
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
@@ -11,6 +18,57 @@ export class PostRepository extends Repository<Post> {
         `There isn't any user with identifier: ${where}`,
       );
     }
+    return post;
+  }
+
+  async updatePost(idx: number, postDto: PostDto) {
+    const post = await this.findOne(idx);
+    if (!post) {
+      throw new NotFoundException(
+        `There isn't any user with identifier: ${idx}`,
+      );
+    }
+    const { title, contents, maxCount } = postDto;
+    await this.update(
+      { post_idx: idx },
+      { title: title, contents: contents, maxCount: maxCount },
+    );
+  }
+
+  async getPeopleList(idx: number) {
+    const peopleList: string[] = await getRepository(Organization)
+      .createQueryBuilder('o')
+      .select('u.name', 'name')
+      .innerJoin('o.user', 'u')
+      .where('o.organization_post = :id', { id: idx })
+      .getRawMany();
+    if (!peopleList) {
+      throw new NotFoundException(
+        `There isn't any user with identifier: ${idx}`,
+      );
+    }
+    return peopleList;
+  }
+
+  async deletePost(idx: number) {
+    const post: Post = await this.findOne(idx);
+    if (!post) {
+      throw new NotFoundException(
+        `There isn't any user with identifier: ${idx}`,
+      );
+    }
+    await this.delete(post);
+  }
+
+  async findOnePost(idx: number) {
+    const post = await getRepository(Post)
+      .createQueryBuilder('p')
+      .select(
+        'p.post_idx, p.title, p.contents, p.isDayOrNight, p.createdAt, p.maxCount, u.name "author"',
+      )
+      .innerJoin('p.user', 'u')
+      .where('p.post_idx = :id', { id: idx })
+      .execute();
     return post;
   }
 }
