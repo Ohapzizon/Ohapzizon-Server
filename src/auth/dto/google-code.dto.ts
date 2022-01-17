fc517bb23d34593ab25db77183a77bf3e615e778
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-01-17 16:00:24 +0900

    :pencil: (swagger) ApiProperty와 validation을 추가하였습니다.

--- src/auth/dto/google-code.dto.ts
+++ src/auth/dto/google-code.dto.ts
@@ -1,6 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GoogleCodeDto {
+  @ApiProperty({
+    description: '코드',
+    required: true,
+  })
+  @IsNotEmpty({ message: '인증 코드가 입력되지 않았습니다.' })
   @IsString()
   readonly code: string;
 }
