d98a2212cf15b16e320e99521116b14e71587a3e
hyunin <hyunin0102@gmail.com>
2022-01-24 19:23:56 +0900

    :sparkles: (OrganizationRepository) findUser method create
    이미 신청한 유저가 다시 신청했는 지 확인하는 메소드

--- src/organization/organization.repository.ts
+++ src/organization/organization.repository.ts
@@ -1,5 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
 import Organization from '../entities/organization.entity';
+import { UserAlreadyExistsException } from '../config/exception/user-already-exists.exception';
 
 @EntityRepository(Organization)
-export class OrganizationRepository extends Repository<Organization> {}
+export class OrganizationRepository extends Repository<Organization> {
+  async findUser(user_: string) {
+    const organization: Organization = await this.findOne({
+      where: { user: user_ },
+      relations: ['user'],
+    });
+    if (organization) {
+      if (organization.user.user_idx == user_) {
+        throw new UserAlreadyExistsException();
+      }
+    }
+  }
+}
