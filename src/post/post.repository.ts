ed7ad4fddf1da48af33045ae29e82cfbb04ed11e
hyunin <hyunin0102@gmail.com>
2022-01-24 18:15:33 +0900

    :fire: (PostRepository) 같은 기능을 하는 메소드를 제거하였습니다.

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -7,7 +7,6 @@ import {
 import Post from '../entities/post.entity';
 import { NotFoundException } from '@nestjs/common';
 import Organization from '../entities/organization.entity';
-import { PostDto } from './dto/post.dto';
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
@@ -17,20 +16,6 @@ export class PostRepository extends Repository<Post> {
       throw new NotFoundException(`해당하는 게시글이 존재하지않습니다.`);
     }
     return post;
-  }
-
-  async updatePost(idx: number, postDto: PostDto) {
-    const post = await this.findOne(idx);
-    if (!post) {
-      throw new NotFoundException(
-        `There isn't any user with identifier: ${idx}`,
-      );
-    }
-    const { title, contents, maxCount } = postDto;
-    await this.update(
-      { post_idx: idx },
-      { title: title, contents: contents, maxCount: maxCount },
-    );
   }
 
   async getPeopleList(idx: number) {
@@ -48,18 +33,8 @@ export class PostRepository extends Repository<Post> {
     return peopleList;
   }
 
-  async deletePost(idx: number) {
-    const post: Post = await this.findOne(idx);
-    if (!post) {
-      throw new NotFoundException(
-        `There isn't any user with identifier: ${idx}`,
-      );
-    }
-    await this.delete(post);
-  }
-
   async findOnePost(idx: number) {
-    const post = await getRepository(Post)
+    return await getRepository(Post)
       .createQueryBuilder('p')
       .select(
         'p.post_idx, p.title, p.contents, p.isDayOrNight, p.createdAt, p.maxCount, u.name "author"',
@@ -67,6 +42,5 @@ export class PostRepository extends Repository<Post> {
       .innerJoin('p.user', 'u')
       .where('p.post_idx = :id', { id: idx })
       .execute();
-    return post;
   }
 }
