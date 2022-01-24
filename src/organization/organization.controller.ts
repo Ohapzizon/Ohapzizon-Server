d15a2c631f982472868ea18d841c6dd64c53cff3
hyunin <hyunin0102@gmail.com>
2022-01-24 19:25:32 +0900

    :recycle: (OrganizationController) add api tags, participate refactoring

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { OrganizationService } from './organization.service';
-import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
+@ApiTags('Organization')
 @Controller('organization')
 export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
@@ -19,9 +33,9 @@ export class OrganizationController {
     description: '성공',
   })
   @HttpCode(HttpStatus.OK)
-  @Get('/participate/:id')
-  async participate(@Param('id') id: string) {
-    const data = await this.organizationService.participate(id);
+  @Get('/participate/:idx')
+  async participate(@Param('idx') idx: number) {
+    const data = await this.organizationService.participate(idx);
     return {
       status: 200,
       message: '땡겨~',
