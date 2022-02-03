05b830b1e77fbcd0877f305867c3a74b560df39e
hyunin <hyunin0102@gmail.com>
2022-02-03 22:39:06 +0900

    :fire: (GroupService) 사용하지 않는 코드 삭제

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { GroupRepository } from './group.repository';
 
 @Injectable()
-export class GroupService {
-  constructor(private readonly groupRepository: GroupRepository) {}
-
-  async saveGroup() {
-    const group = this.groupRepository.create();
-    return this.groupRepository.save(group);
-  }
-}
+export class GroupService {}
