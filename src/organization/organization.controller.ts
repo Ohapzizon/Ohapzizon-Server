4c15fc6cb9db3b5cc65a684fb99ed15ac10ec6c3
hyunin <hyunin0102@gmail.com>
2022-02-03 22:40:48 +0900

    :triangular_flag_on_post: (OrganizationController) add UseGuards

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,41 +1,44 @@
 import {
   Controller,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
+  ParseIntPipe,
   UseGuards,
 } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import {
   ApiBearerAuth,
+  ApiOkResponse,
   ApiOperation,
   ApiParam,
-  ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import User from '../entities/user.entity';
+import { UserDecorator } from '../common/decorators/user.decorator';
 
 @ApiTags('Organization')
 @Controller('organization')
 export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
 
-  @ApiBearerAuth('accessToken')
-  @UseGuards(JwtAuthGuard)
   @ApiParam({
-    name: 'idx',
+    name: 'id',
     required: true,
     description: '땡길 게시글',
   })
   @ApiOperation({ summary: '땡겨' })
-  @ApiResponse({
+  @ApiOkResponse({
     description: '성공',
   })
-  @HttpCode(HttpStatus.OK)
-  @Get('/participate/:idx')
-  async participate(@Param('idx') idx: number) {
-    const data = await this.organizationService.participate(idx);
+  @ApiBearerAuth('accessToken')
+  @UseGuards(JwtAuthGuard)
+  @Get('/participate/:id')
+  async participate(
+    @Param('id', ParseIntPipe) id: number,
+    @UserDecorator() user: User,
+  ) {
+    const data = await this.organizationService.participate(id, user);
     return {
       status: 200,
       message: '땡겨~',
