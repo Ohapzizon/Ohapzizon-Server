b5308a82105d7590f9e64168d5c8813b7519c1b2
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-04-13 23:59:25 +0900

    :bug: (AuthGuard) 비동기 처리를 위한 await구문을 추가하였습니다.

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -12,14 +12,14 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
     if (authorization === undefined) {
       throw new BadRequestException('Authorization 헤더가 비어있습니다.');
     }
-    const accessToken = authorization.split(' ')[1];
-    request.user = this.tokenService.validateToken(accessToken); // request.user 객체에 디코딩된 토큰(유저 정보)을 저장합니다.
+    const accessToken = authorization.split(' ')[2];
+    request.user = await this.tokenService.validateToken(accessToken); // request.user 객체에 디코딩된 토큰(유저 정보)을 저장합니다.
     return true;
   }
 }
