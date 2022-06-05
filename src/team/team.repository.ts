bb82145a4c25201d0070686d717661ce3a014427
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-06-06 01:13:37 +0900

    :recycle: (TeamRepository) findShowTeamPostDto method 캡슐화

--- src/team/team.repository.ts
+++ src/team/team.repository.ts
@@ -1,29 +1,28 @@
-import { EntityRepository, Repository, SelectQueryBuilder } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import Team from '../entities/team.entity';
 import { plainToClass } from 'class-transformer';
 import { ShowTeamDto } from './dto/show-team.dto';
 
 @EntityRepository(Team)
 export class TeamRepository extends Repository<Team> {
-  async findByUserId(userId: string): Promise<ShowTeamDto> {
-    const row = await this.findTeam()
-      .where('p.userId = :userId', { userId: userId })
-      .getOne();
-    return plainToClass(ShowTeamDto, row);
-  }
-
-  async findByPostIdx(postIdx: number): Promise<ShowTeamDto[]> {
-    const row = await this.findTeam()
-      .where('p.postIdx = :postIdx', { postIdx: postIdx })
+  async findShowTeamDtoByPostIdx(postIdx: number): Promise<ShowTeamDto[]> {
+    const row = await this.findShowTeamPostDto()
+      .where('p.idx = :postIdx', { postIdx: postIdx })
       .getMany();
     return plainToClass(ShowTeamDto, row);
   }
 
-  private findTeam(): SelectQueryBuilder<Team> {
+  async findShowTeamDtoByUserId(userId: string): Promise<ShowTeamDto> {
+    const row = await this.findShowTeamPostDto()
+      .where('u.userId = :userId', { userId: userId })
+      .getOne();
+    return plainToClass(ShowTeamDto, row);
+  }
+
+  private findShowTeamPostDto() {
     return this.createQueryBuilder('t')
-      .select(['t.teamIdx', 't.status'])
-      .addSelect('u.name', 'participants')
-      .innerJoin('t.user', 'u')
+      .select(['t.idx', 't.status'])
+      .innerJoinAndSelect('t.participants', 'u')
       .innerJoin('t.post', 'p');
   }
 }
