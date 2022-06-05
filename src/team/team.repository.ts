7107df63fb6fad28bd4d89667253b455d0a8835b
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-06-06 01:14:19 +0900

    :fire: (TeamRepository) 사용하지 않는 코드 삭제

--- src/team/team.repository.ts
+++ src/team/team.repository.ts
@@ -1,28 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import Team from '../entities/team.entity';
-import { plainToClass } from 'class-transformer';
-import { ShowTeamDto } from './dto/show-team.dto';
 
 @EntityRepository(Team)
-export class TeamRepository extends Repository<Team> {
-  async findShowTeamDtoByPostIdx(postIdx: number): Promise<ShowTeamDto[]> {
-    const row = await this.findShowTeamPostDto()
-      .where('p.idx = :postIdx', { postIdx: postIdx })
-      .getMany();
-    return plainToClass(ShowTeamDto, row);
-  }
-
-  async findShowTeamDtoByUserId(userId: string): Promise<ShowTeamDto> {
-    const row = await this.findShowTeamPostDto()
-      .where('u.userId = :userId', { userId: userId })
-      .getOne();
-    return plainToClass(ShowTeamDto, row);
-  }
-
-  private findShowTeamPostDto() {
-    return this.createQueryBuilder('t')
-      .select(['t.idx', 't.status'])
-      .innerJoinAndSelect('t.participants', 'u')
-      .innerJoin('t.post', 'p');
-  }
-}
+export class TeamRepository extends Repository<Team> {}
