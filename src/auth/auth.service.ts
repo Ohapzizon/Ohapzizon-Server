d99fc2cdeef8ad728007e4f3b67c02192b691129
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-06-30 23:27:05 +0900

    :recycle: (AuthService) 함수명 변경 및 로직 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { GoogleCodeDto } from './dto/google-code.dto';
 import axios, { AxiosResponse } from 'axios';
-import { IGoogleUser } from './types/google-user.types';
 import { ConfigService } from '@nestjs/config';
 import { TokenService } from '../token/token.service';
 import { LoginDto } from './dto/login.dto';
 import { UserService } from '../user/user.service';
 import User from '../entities/user.entity';
+import { GoogleUserInfo } from './types/google-user.types';
 
 @Injectable()
 export class AuthService {
@@ -24,17 +24,34 @@ export class AuthService {
   private readonly callbackURL: string =
     this.configService.get<string>('CALLBACK_URL');
 
-  async logIn(googleCodeDto: GoogleCodeDto): Promise<LoginDto> {
-    const data: IGoogleUser = await this.getGoogleUserInfo(googleCodeDto);
-    const user: User = await this.userService.register({
-      googleId: data.id,
-      email: data.email,
-      name: data.name,
-    });
-    const tokens: Map<string, string> = await this.tokenService.createTokens(
-      user,
-    );
-    return new LoginDto(user.name, tokens);
+  getGoogleRedirectURL() {
+    const hostName = 'https://accounts.google.com';
+    const responseType = 'code';
+    const scope = 'email profile';
+    return {
+      url: `${hostName}/o/oauth2/v2/auth/oauthchooseaccount?response_type=${responseType}&redirect_uri=${this.callbackURL}&scope=${scope}&client_id=${this.clientID}`,
+      statusCode: 302,
+    };
+  }
+
+  async googleLogIn(googleCodeDto: GoogleCodeDto): Promise<LoginDto> {
+    try {
+      const googleUserInfo: GoogleUserInfo = await this.getGoogleUserInfo(
+        googleCodeDto,
+      );
+      const registeredUser: User = await this.userService.register({
+        googleId: googleUserInfo.id,
+        email: googleUserInfo.email,
+        name: googleUserInfo.name,
+      });
+      const map: Map<string, string> = await this.tokenService.createTokens(
+        registeredUser,
+      );
+      return new LoginDto(map);
+    } catch (e) {
+      this.logger.error(e);
+      throw new UnauthorizedException('회원가입에 실패하였습니다.');
+    }
   }
 
   logOut(userId: number): Promise<void> {
@@ -43,7 +60,7 @@ export class AuthService {
 
   private async getGoogleUserInfo(
     googleCodeDto: GoogleCodeDto,
-  ): Promise<IGoogleUser> {
+  ): Promise<GoogleUserInfo> {
     const { code } = googleCodeDto;
     const getTokenUrl = `https://oauth2.googleapis.com/token?code=${code}&client_id=${this.clientID}&client_secret=${this.clientSecret}&redirect_uri=${this.callbackURL}&grant_type=authorization_code`;
     try {
