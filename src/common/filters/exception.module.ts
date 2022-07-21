6c4324a3e973cd65db8202d529099b607a255284
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-07-21 23:20:21 +0900

    :fire: (ExceptionModule) Removing unused file

--- src/common/filters/exception.module.ts
+++
@@ -1,8 +0,0 @@
-import { Logger, Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
-import { GlobalExceptionFilter } from './global-exception.filter';
-
-@Module({
-  providers: [Logger, { provide: APP_FILTER, useClass: GlobalExceptionFilter }],
-})
-export class ExceptionModule {}
