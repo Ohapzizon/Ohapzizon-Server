b7a64d7e8a742914c7467345b7520527e1877772
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-10-28 13:30:39 +0900

    ✨ (Guard) 토큰 검증 로직 변경 및 RegisterGuard 추가

---
+++ src/auth/guard/social-register-auth.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { SocialRegisterTokenData } from '../../token/types/tokenData';
+import { validateToken } from '../../token/lib/token';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class SocialRegisterAuthGuard implements CanActivate {
+  constructor(private readonly configService: ConfigService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const { register_token } = req.headers;
+    if (!register_token)
+      throw new UnauthorizedException('Register Token 헤더가 비어있습니다.');
+    let registerToken = register_token.split(' ')[1];
+    const REGISTER_TOKEN_SECRET = this.configService.get<string>(
+      'REGISTER_TOKEN_SECRET',
+    );
+    registerToken = await validateToken<SocialRegisterTokenData>(
+      registerToken,
+      REGISTER_TOKEN_SECRET,
+    );
+    req.register_token = registerToken;
+    return true;
+  }
+}
