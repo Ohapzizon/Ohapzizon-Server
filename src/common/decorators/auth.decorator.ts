8b2c1eea7fe498f22e218640cca1da9590993da1
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-10-28 13:34:29 +0900

    📄 (Swagger) AuthDecorator
    ApiUnauthorizedResponse, ApiForbiddenResponse 추가

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,14 +1,27 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { RoleGuard } from '../../auth/guard/role.guard';
-import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Role } from '../../user/enum/role';
+import { InvalidTokenError } from '../response/swagger/error/invalid-token.error';
+import { ForbiddenError } from '../response/swagger/error/forbidden.error';
 
 export function Auth(...role: Role[]) {
   return applyDecorators(
     SetMetadata('role', role),
     UseGuards(JwtAuthGuard, RoleGuard),
     ApiBearerAuth('accessToken'),
-    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+    ApiUnauthorizedResponse({
+      description: 'Invalid Token',
+      type: InvalidTokenError,
+    }),
+    ApiForbiddenResponse({
+      description: 'Forbidden',
+      type: ForbiddenError,
+    }),
   );
 }
