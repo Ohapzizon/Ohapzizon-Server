fdee1b6cdd43edcd6f71da43976f781155c2cddb
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-10-29 09:48:20 +0900

    Update index.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,5 +1,8 @@
 import User from './user.entity';
 import Post from './post.entity';
 import Team from './team.entity';
+import AuthToken from './auth-token.entity';
+import SocialAccount from './social-account.entity';
+import UserProfile from './user-profile.entity';
 
-export default [User, Post, Team];
+export default [User, Post, Team, AuthToken, SocialAccount, UserProfile];
