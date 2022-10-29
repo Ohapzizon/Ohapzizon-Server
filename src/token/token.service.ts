83ad281c5d054bd250e44724af7f6818a631aff3
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-10-29 10:01:00 +0900

    Update token.service.ts

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,85 +1,153 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
-import { UserService } from '../user/user.service';
-import * as bcrypt from 'bcrypt';
+import { RefreshTokenData } from './types/tokenData';
 import User from '../entities/user.entity';
-import { ReissuanceDto } from './dto/reissuance.dto';
+import AuthToken from '../entities/auth-token.entity';
+import { TokenDto } from './dto/token.dto';
+import dataSource from '../config/database/data-source';
+import { EntityManager } from 'typeorm';
+import { Response } from 'express';
+import { UserService } from '../user/user.service';
+import { authTokenRepository } from './token.repository';
+import UserProfile from '../entities/user-profile.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TokenService {
   constructor(
-    readonly configService: ConfigService,
     private readonly userService: UserService,
+    private readonly configService: ConfigService,
   ) {}
 
-  private TOKEN_SECRET = this.configService.get<string>('TOKEN_SECRET');
-  private ACCESS_TOKEN_EXPIRED = this.configService.get<string>(
+  private ACCESS_TOKEN_SECRET = this.configService.get<string>(
+    'ACCESS_TOKEN_SECRET',
+  );
+  private REFRESH_TOKEN_SECRET = this.configService.get<string>(
+    'REFRESH_TOKEN_SECRET',
+  );
+  private REGISTER_TOKEN_SECRET = this.configService.get<string>(
+    'REGISTER_TOKEN_SECRET',
+  );
+  private ACCESS_TOKEN_EXPIRATION = +this.configService.get<number>(
     'ACCESS_TOKEN_EXPIRATION_TIME',
   );
-  private REFRESH_TOKEN_EXPIRED = this.configService.get<string>(
+  private REFRESH_TOKEN_EXPIRATION = +this.configService.get<number>(
     'REFRESH_TOKEN_EXPIRATION_TIME',
   );
+  private REGISTER_TOKEN_EXPIRATION = +this.configService.get<number>(
+    'REGISTER_TOKEN_EXPIRATION_TIME',
+  );
 
-  async createTokens(user: User): Promise<Map<string, string>> {
-    const map = new Map<string, string>();
-    const accessToken: string = this.createAccessToken(
-      user.googleId,
-      user.name,
-      user.role,
+  async generateUserToken(
+    user: User,
+    userProfile: UserProfile,
+    entityManger: EntityManager,
+  ): Promise<TokenDto> {
+    const accessToken = this.generateAccessToken({
+      sub: user.id,
+      name: user.name,
+      nickname: userProfile.displayName,
+      email: user.email,
+      role: user.role,
+    });
+    let authToken: AuthToken | null = await authTokenRepository.findOneByUserId(
+      user.id,
     );
-    const refreshToken: string = await this.createRefreshToken(user.userId);
-    map.set('username', user.name);
-    map.set('accessToken', 'Bearer ' + accessToken);
-    map.set('refreshToken', 'Bearer ' + refreshToken);
-    return map;
+    if (!authToken) {
+      authToken = authTokenRepository.create({
+        id: uuidv4(),
+        user: { id: user.id },
+      });
+    }
+    const refreshToken = this.generateRefreshToken({
+      sub: authToken.id,
+    });
+    await entityManger.insert(AuthToken, authToken);
+    return new TokenDto(accessToken, refreshToken);
   }
 
-  async reissuanceToken(userId: number): Promise<ReissuanceDto> {
-    const user: User = await this.userService.findByUserId(userId);
-    const token = this.createAccessToken(user.googleId, user.name, user.role);
-    return new ReissuanceDto(token);
+  async refreshUserToken(
+    user: User,
+    userProfile: UserProfile,
+    refreshTokenData: RefreshTokenData,
+    res: Response,
+  ): Promise<TokenDto> {
+    let authToken: AuthToken;
+    try {
+      authToken = await authTokenRepository.findOneByIdOrFail(
+        refreshTokenData.sub,
+      );
+    } catch (e) {
+      throw new UnauthorizedException('Invalid Token');
+    }
+    const accessToken = this.generateAccessToken({
+      sub: user.id,
+      name: user.name,
+      nickname: userProfile.displayName,
+      email: user.email,
+      role: user.role,
+    });
+    const now = new Date().getTime();
+    const diff = refreshTokenData.exp * 1000 - now;
+    if (diff < 1000 * 60 * 60 * 24 * 23) {
+      const refreshToken = this.generateRefreshToken({
+        sub: authToken.id,
+      });
+      this.setTokenCookie(res, accessToken, refreshToken);
+      return new TokenDto(accessToken, refreshToken);
+    }
+    this.setTokenCookie(res, accessToken);
+    return new TokenDto(accessToken);
   }
 
-  removeRefreshToken(userId: number): Promise<void> {
-    return this.userService.updateRefreshTokenById(userId, null);
+  async disabledAuthTokenByUserId(userId: string): Promise<void> {
+    const authToken: AuthToken = await dataSource
+      .createQueryBuilder(AuthToken, 'a')
+      .where('u.id = :id', { id: userId })
+      .innerJoin('a.user', 'u')
+      .getOneOrFail();
+    authToken.disabled = true;
+    await dataSource.manager.save(AuthToken, authToken);
   }
 
-  private createAccessToken(
-    googleId: string,
-    name: string,
-    role: string,
-  ): string {
-    const payload = {
-      googleId: googleId,
-      name: name,
-      role: role,
-    };
-    return jwt.sign(payload, this.TOKEN_SECRET, {
-      expiresIn: this.ACCESS_TOKEN_EXPIRED + 'm',
+  generateRegisterToken(payload: any): string {
+    return jwt.sign(payload, this.REGISTER_TOKEN_SECRET, {
+      issuer: 'ohapzizon.com',
       algorithm: 'HS256',
+      expiresIn: this.REGISTER_TOKEN_EXPIRATION,
     });
   }
 
-  private async createRefreshToken(userId: number): Promise<string> {
-    const payload = {};
-    const refreshToken = jwt.sign(payload, this.TOKEN_SECRET, {
-      expiresIn: this.REFRESH_TOKEN_EXPIRED + 'm',
+  private generateAccessToken(payload: any): string {
+    return jwt.sign(payload, this.ACCESS_TOKEN_SECRET, {
+      issuer: 'ohapzizon.com',
       algorithm: 'HS256',
+      expiresIn: this.ACCESS_TOKEN_EXPIRATION,
     });
-    await this.setHashedRefreshToken(refreshToken, userId);
-    return refreshToken;
   }
 
-  private async setHashedRefreshToken(
-    refreshToken: string,
-    userId: number,
-  ): Promise<void> {
-    const salt = await bcrypt.genSalt();
-    const currentHashedRefreshToken = await bcrypt.hash(refreshToken, salt);
-    return this.userService.updateRefreshTokenById(
-      userId,
-      currentHashedRefreshToken,
-    );
+  private generateRefreshToken(payload: any): string {
+    return jwt.sign(payload, this.REFRESH_TOKEN_SECRET, {
+      issuer: 'ohapzizon',
+      algorithm: 'HS256',
+      expiresIn: this.REFRESH_TOKEN_EXPIRATION,
+    });
+  }
+
+  setTokenCookie(res: Response, accessToken: string, refreshToken?: string) {
+    res.cookie('accessToken', accessToken, {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+    });
+    res.cookie('refreshToken', refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
+  }
+
+  async resetTokenCookie(res: Response) {
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
   }
 }
