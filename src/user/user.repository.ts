b0c49cc8a2f7e7cac07dc6f789bdcfdb2473d6e5
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-10-29 10:55:40 +0900

    Update user.repository.ts

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,5 +1,22 @@
-import { EntityRepository, Repository } from 'typeorm';
+import dataSource from '../config/database/data-source';
 import User from '../entities/user.entity';
+import { isExistQuery } from '../common/row-query/is-exists';
 
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export const userRepository = dataSource.getRepository(User).extend({
+  async findOneByIdOrFail(userId: string): Promise<User> {
+    return this.createQueryBuilder('u')
+      .innerJoinAndSelect('u.profile', 'p')
+      .where('u.id = :id', { id: userId })
+      .getOneOrFail();
+  },
+
+  async isExistByEmail(email: string): Promise<boolean> {
+    return isExistQuery(
+      this.createQueryBuilder('u')
+        .select('u.id')
+        .where('u.email = ?')
+        .getQuery(),
+      [`${email}`],
+    );
+  },
+});
