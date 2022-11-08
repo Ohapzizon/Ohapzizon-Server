7be3cdb3e8dc75f595dcdf925f9b9eadd53dea76
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-11-08 15:10:56 +0900

    :recycle: (AuthService) Refactoring SocialRegister method
    entityManager를 통한 repository 호출 방법 변경 및 authToken을 transaction에 포함

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { TokenService } from '../token/token.service';
 import { SocialRegisterTokenData } from '../token/types/token-data';
-import { RegisterUserProfileDto } from '../user/dto/register-user-profile.dto';
+import { RegisterUserProfileDto } from './dto/register-user-profile.dto';
 import SocialAccount from '../entities/social-account.entity';
 import { TokenDto } from '../token/dto/token.dto';
-import { SocialProfile } from './social/types/social-profile';
 import { UserService } from '../user/user.service';
 import dataSource from '../config/database/data-source';
 import { LoginDto } from './dto/login.dto';
 import { Response } from 'express';
+import User from '../entities/user.entity';
+import UserProfile from '../entities/user-profile.entity';
+import { v4 as uuidv4 } from 'uuid';
+import AuthToken from '../entities/auth-token.entity';
+import { userRepository } from '../user/user.repository';
+import { userProfileRepository } from '../user/user-profile/user-profile.repository';
+import { socialAccountRepository } from './social/social-account.repository';
+import { authTokenRepository } from '../token/token.repository';
 
 @Injectable()
 export class AuthService {
@@ -18,36 +25,48 @@ export class AuthService {
   ) {}
 
   async socialRegister(
-    socialRegisterTokenData: SocialRegisterTokenData,
+    { profile, provider, accessToken }: SocialRegisterTokenData,
     registerUserDto: RegisterUserProfileDto,
     res: Response,
   ): Promise<LoginDto> {
-    const profile: SocialProfile = socialRegisterTokenData.profile;
     return await dataSource.transaction(async (transactionalEntityManager) => {
-      const { user, userProfile } = await this.userService.register(
-        profile,
-        registerUserDto,
-        transactionalEntityManager,
+      const existedUser: boolean = await this.userService.isExistByEmail(
+        profile.email,
       );
-      const socialAccountRepository =
-        transactionalEntityManager.getRepository(SocialAccount);
-      const socialAccount: SocialAccount = socialAccountRepository.create({
-        socialId: socialRegisterTokenData.profile.id,
-        provider: socialRegisterTokenData.provider,
-        accessToken: socialRegisterTokenData.accessToken,
+      if (existedUser) throw new BadRequestException('User Is Already Exists');
+      const user: User = userRepository.create({
+        id: uuidv4(),
+        email: profile.email,
+        name: profile.name,
+      });
+      const userProfile: UserProfile = userProfileRepository.create({
+        displayName: registerUserDto.displayName,
+        discordTag: registerUserDto.discordTag,
+        grade: registerUserDto.grade,
+        department: registerUserDto.department,
+        thumbnail: profile.thumbnail,
         user: { id: user.id },
       });
-      await socialAccountRepository.save(socialAccount);
+      const socialAccount: SocialAccount = socialAccountRepository.create({
+        socialId: profile.id,
+        provider: provider,
+        accessToken: accessToken,
+        user: { id: user.id },
+      });
+      const authToken = authTokenRepository.create({
+        id: uuidv4(),
+        user: { id: user.id },
+      });
+      await transactionalEntityManager.save(User, user);
+      await transactionalEntityManager.save(UserProfile, userProfile);
+      await transactionalEntityManager.save(SocialAccount, socialAccount);
+      await transactionalEntityManager.save(AuthToken, authToken);
       const tokenDto: TokenDto = await this.tokenService.generateUserToken(
         user,
         userProfile,
-        transactionalEntityManager,
+        authToken,
       );
-      this.tokenService.setTokenCookie(
-        res,
-        tokenDto.accessToken,
-        tokenDto.refreshToken,
-      );
+      this.tokenService.setTokenCookie(res, tokenDto);
       return new LoginDto(userProfile, tokenDto);
     });
   }
