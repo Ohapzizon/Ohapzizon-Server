dd39e958b4f8f0a4a32954e671b0c4562c58144e
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-11-10 08:39:36 +0900

    ✨ (SocialProfileDto) validation decorator 추가

--- src/auth/dto/social-profile.dto.ts
+++ src/auth/dto/social-profile.dto.ts
@@ -1,17 +1,21 @@
 import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class SocialProfileDto {
   @ApiProperty({ example: '114259175033980717742' })
   @Expose()
+  @IsNotEmpty()
   socialId: string;
 
   @ApiProperty({ example: '송유현' })
   @Expose()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ example: 'doong3373@gmail.com' })
   @Expose()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty({
@@ -19,5 +23,6 @@ export class SocialProfileDto {
       'https://lh3.googleusercontent.com/a/ALm5wu2wfmlLoAS0UDsflc_pFbRz09LsGW_P30Y9uY-r=s96-c',
   })
   @Expose()
-  thumbnail: string;
+  @IsOptional()
+  thumbnail?: string | null;
 }
