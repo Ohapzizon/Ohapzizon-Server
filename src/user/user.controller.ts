a9d1bcb2add8731dcf2fee562bc5607dc0d1c403
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-11-10 08:59:18 +0900

    :recycle: (UserService) EndPoint 변경

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { PostService } from '../post/post.service';
 import {
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
@@ -6,16 +7,18 @@ import {
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
-import { Role } from './enum/role';
+import { FindMyJoinedPostResponse } from '../team/res/find-my-joined-post.response';
 import { Auth } from '../common/decorators/auth.decorator';
-import { UserService } from './user.service';
 import { AccessToken } from '../common/decorators/token.decorator';
 import { ResponseEntity } from '../common/response/response.entity';
-import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
-import { ShowUserProfileDto } from './dto/show-user-profile.dto';
-import { FindUserResponse } from './res/find-user.response';
-import { InternalServerError } from '../common/response/swagger/error/internal-server.error';
+import { ShowPostDto } from '../post/dto/show-post.dto';
+import { UserProfileService } from './user-profile/user-profile.service';
+import { FindUserProfileResponse } from './user-profile/res/find-user-profile.response';
 import { NotFoundError } from '../common/response/swagger/error/not-found.error';
+import UserProfile from '../entities/user-profile.entity';
+import { UpdateUserProfileDto } from './user-profile/dto/update-user-profile.dto';
+import { InternalServerError } from '../common/response/swagger/error/internal-server.error';
+import { UpdateUserProfileResponse } from './user-profile/res/update-user-profile.response';
 
 @ApiInternalServerErrorResponse({
   description: '서버 에러입니다.',
@@ -24,34 +27,65 @@ import { NotFoundError } from '../common/response/swagger/error/not-found.error'
 @ApiTags('user')
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly userProfileService: UserProfileService,
+  ) {}
+
+  @ApiOperation({
+    summary: '내 프로필 수정',
+    description: '자신의 프로필을 수정합니다.',
+  })
+  @ApiOkResponse({
+    description: '프로필 수정에 성공하였습니다.',
+    type: UpdateUserProfileResponse,
+  })
+  @Auth()
+  @Patch('profile')
+  async updateProfile(
+    @AccessToken('sub') userId: string,
+    @Body() updateUserDto: UpdateUserProfileDto,
+  ) {
+    await this.userProfileService.updateProfileByUserId(userId, updateUserDto);
+    return ResponseEntity.OK_WITH('프로필 수정에 성공하였습니다.');
+  }
 
   @ApiOperation({ summary: '프로필 조회' })
   @ApiOkResponse({
     description: '프로필 조회에 성공하였습니다.',
-    type: FindUserResponse,
+    type: FindUserProfileResponse,
   })
   @ApiNotFoundResponse({
     description: '요청하신 자료를 찾을 수 없습니다.',
     type: NotFoundError,
   })
-  @Get(':userProfileId')
+  @Get('profile/:userProfileId')
   async findUserProfile(
     @Param('userProfileId') userProfileId: string,
-  ): Promise<ResponseEntity<ShowUserProfileDto>> {
-    const data: ShowUserProfileDto =
-      await this.userService.findUserProfileByIdOrFail(userProfileId);
+  ): Promise<ResponseEntity<UserProfile>> {
+    const data: UserProfile = await this.userProfileService.findOneByIdOrFail(
+      userProfileId,
+    );
     return ResponseEntity.OK_WITH_DATA('프로필 조회에 성공하였습니다.', data);
   }
 
-  @ApiOperation({ summary: '내 프로필 수정' })
-  @Auth(Role.USER)
-  @Patch()
-  async updateProfile(
+  @ApiOperation({
+    summary: '자신이 참여한 모집글 조회',
+    description: '자신이 참여한 모집글을 조회합니다.',
+  })
+  @ApiOkResponse({
+    description: '참여한 모집글 조회에 성공하였습니다.',
+    type: FindMyJoinedPostResponse,
+  })
+  @Auth()
+  @Get('post')
+  async findMyJoinedPost(
     @AccessToken('sub') userId: string,
-    @Body() updateUserDto: UpdateUserProfileDto,
-  ) {
-    await this.userService.updateProfileByUserId(userId, updateUserDto);
-    return ResponseEntity.OK_WITH('사용자 정보 수정에 성공하였습니다.');
+  ): Promise<ResponseEntity<ShowPostDto[]>> {
+    const data: ShowPostDto[] = await this.postService.findMyJoinedPost(userId);
+    return ResponseEntity.OK_WITH_DATA(
+      '참여한 모집글 조회에 성공하였습니다.',
+      data,
+    );
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,65 +1,14 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import User from '../entities/user.entity';
-import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
-import UserProfile from '../entities/user-profile.entity';
-import { EntityManager } from 'typeorm';
-import { SocialProfile } from '../auth/social/types/social-profile';
-import { RegisterUserProfileDto } from './dto/register-user-profile.dto';
 import { userRepository } from './user.repository';
-import { userProfileRepository } from './user-profile.repository';
-import { v4 as uuidv4 } from 'uuid';
-import { ShowUserProfileDto } from './dto/show-user-profile.dto';
 
 @Injectable()
 export class UserService {
-  async findOneWithProfileByIdOrFail(userId: string): Promise<User> {
+  async findOneByIdOrFail(userId: string): Promise<User> {
     return userRepository.findOneByIdOrFail(userId);
-  }
-
-  async findUserProfileByIdOrFail(
-    userProfileId: string,
-  ): Promise<ShowUserProfileDto> {
-    const userProfile: UserProfile =
-      await userProfileRepository.findUserProfileByIdOrFail(userProfileId);
-    return new ShowUserProfileDto(userProfile);
   }
 
   async isExistByEmail(email: string): Promise<boolean> {
     return userRepository.isExistByEmail(email);
-  }
-
-  async register(
-    profile: SocialProfile,
-    registerUserProfileDto: RegisterUserProfileDto,
-    entityManager: EntityManager,
-  ): Promise<{ user: User; userProfile: UserProfile }> {
-    const existUser: boolean = await this.isExistByEmail(profile.email);
-    if (existUser) throw new BadRequestException('User Is Already Exists');
-    const user: User = userRepository.create({
-      id: uuidv4(),
-      email: profile.email,
-      name: profile.name,
-    });
-    const userProfile: UserProfile = userProfileRepository.create({
-      displayName: registerUserProfileDto.displayName,
-      discordTag: registerUserProfileDto.discordTag,
-      grade: registerUserProfileDto.grade,
-      department: registerUserProfileDto.department,
-      user: { id: user.id },
-    });
-    if (profile.thumbnail) userProfile.thumbnail = profile.thumbnail;
-    await entityManager.insert(User, user);
-    await entityManager.insert(UserProfile, userProfile);
-    return { user, userProfile };
-  }
-
-  async updateProfileByUserId(
-    userId: string,
-    updateUserProfileDto: UpdateUserProfileDto,
-  ): Promise<void> {
-    const currentUser: User = await this.findOneWithProfileByIdOrFail(userId);
-    currentUser.profile.discordTag = updateUserProfileDto.discordTag;
-    currentUser.profile.displayName = updateUserProfileDto.displayName;
-    await userProfileRepository.save(currentUser.profile);
   }
 }
