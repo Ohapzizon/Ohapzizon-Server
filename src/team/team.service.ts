a9b4d614b4ac00c765ca0648c6e9c2bc6876a847
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-11-10 10:18:10 +0900

    :recycle: (TeamService)

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -2,97 +2,65 @@ import {
   BadRequestException,
   ForbiddenException,
   Injectable,
-  Logger,
 } from '@nestjs/common';
-import { Status } from './enum/status';
 import Team from '../entities/team.entity';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { ShowTeamDto } from './dto/show-team.dto';
-import { ShowPostDto } from '../post/dto/show-post.dto';
 import { teamRepository } from './team.repository';
-import * as _ from 'lodash';
 import Post from '../entities/post.entity';
-import { PostService } from '../post/post.service';
+import { PostStatus } from '../post/enum/post-status';
+import { postRepository } from '../post/post.repository';
+import { UpdateJoinStatusDto } from './dto/update-join-status.dto';
 
 @Injectable()
 export class TeamService {
-  constructor(private readonly postService: PostService) {}
-  async findOneByIdOrFail(teamId: number): Promise<Team> {
-    return teamRepository.findOneByIdOrFail(teamId);
-  }
-
-  async findOneByPostIdAndUserIdOrFail(
-    postId: number,
-    userId: string,
-  ): Promise<Team> {
-    return teamRepository.findOneByPostIdAndUserIdOrFail(postId, userId);
-  }
-
-  async findByPostId(postId: number): Promise<Team[]> {
-    return teamRepository.findByPostId(postId);
-  }
-
-  async isExistById(teamId: number): Promise<boolean> {
-    return teamRepository.isExistById(teamId);
-  }
-
   async findShowTeamDtoByPostId(postId: number): Promise<ShowTeamDto[]> {
-    return await teamRepository
+    return teamRepository
       .findShowTeam()
-      .innerJoin('t.post', 'p')
-      .where('p.id = :id', { id: postId })
+      .where('post.id = :id', { id: postId })
+      .innerJoin('t.post', 'post')
       .getRawMany<ShowTeamDto>();
   }
 
-  async findMyJoinedPost(userId: string): Promise<ShowPostDto[]> {
-    return await teamRepository
-      .findShowPost()
-      .innerJoin('t.user', 'participants')
-      .where('participants.id = :id', { id: userId })
-      .getRawMany<ShowPostDto>();
-  }
-
   async join(
-    postId: number,
+    post: Post,
     userId: string,
     createTeamDto: CreateTeamDto,
   ): Promise<ShowTeamDto[]> {
-    const existTeam: Team[] = await this.findByPostId(postId);
-    if (existTeam.find((team) => _.isEqual(team.user.id, userId)))
-      throw new BadRequestException('이미 참가한 사용자입니다.');
-    // TODO: post를 조회하지 않고, 참가 모집 마감을 확인하는 pipe를 제작
-    // TODO: exist pipe에서 postId를 조회하지 않고 모집 마감 상태을 조회
-    const post = await this.postService.findOneByIdOrFail(postId);
-    if (post.limit <= existTeam.length)
-      throw new BadRequestException('이미 참가 모집이 마감된 글입니다.');
-    const team: Team = teamRepository.create({
-      bio: createTeamDto.bio,
-      user: { id: userId },
-      post: { id: postId },
-    });
-    await teamRepository.save(team);
-    return this.findShowTeamDtoByPostId(postId);
-  }
-
-  async updateStatus(
-    teamId: number,
-    userId: string,
-    status: Status,
-  ): Promise<void> {
-    const team: Team = await this.findOneByIdOrFail(teamId);
-    if (!_.isEqual(team.post.writer.id, userId))
-      throw new ForbiddenException('작성자가 아닙니다.');
-    await teamRepository.update(
-      { user: { id: team.user.id } },
-      { status: status },
-    );
-  }
-
-  async cancelJoinRequest(postId: number, userId: string): Promise<void> {
-    const team: Team = await this.findOneByPostIdAndUserIdOrFail(
-      postId,
+    if (post.status === PostStatus.CLOSED)
+      throw new BadRequestException('참가 모집이 마감된 글입니다.');
+    const existTeam: boolean = await teamRepository.isExistByPostIdAndUserId(
+      post.id,
       userId,
     );
+    if (existTeam) throw new BadRequestException('이미 참가한 사용자입니다.');
+    const team: Team = teamRepository.create({
+      bio: createTeamDto.bio,
+      post: { id: post.id },
+      user: { id: userId },
+    });
+    await teamRepository.save(team);
+    const count = await teamRepository.countBy({ post: { id: post.id } });
+    if (post.limit <= count) {
+      post.status = PostStatus.CLOSED;
+      await postRepository.save(post);
+    }
+    return this.findShowTeamDtoByPostId(post.id);
+  }
+
+  async updateJoinStatus(
+    teamId: number,
+    updateJoinStatusDto: UpdateJoinStatusDto,
+  ): Promise<void> {
+    const team: Team = await teamRepository.findOneByIdOrFail(teamId);
+    team.status = updateJoinStatusDto.joinStatus;
+    await teamRepository.save(team);
+  }
+
+  async cancelJoinRequest(teamId: number, userId: string): Promise<void> {
+    const team: Team = await teamRepository.findOneWithUserByIdOrFail(teamId);
+    if (team.user.id !== userId)
+      throw new ForbiddenException('신청자가 아닙니다.');
     await teamRepository.delete({ id: team.id });
   }
 }
