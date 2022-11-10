1c60340fc334e23b42853ed562c528a05ea42419
송유현 <68471917+songyouhyun@users.noreply.github.com>
2022-11-10 10:05:17 +0900

    :sparkles: (postByIdPipe)

---
+++ src/post/pipe/post-by-id.pipe.ts
@@ -0,0 +1,11 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import Post from '../../entities/post.entity';
+import { PostService } from '../post.service';
+
+@Injectable()
+export class postByIdPipe implements PipeTransform<number, Promise<Post>> {
+  constructor(private readonly postService: PostService) {}
+  async transform(value: number, metadata: ArgumentMetadata): Promise<Post> {
+    return this.postService.findOneByIdOrFail(value);
+  }
+}
